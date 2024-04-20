670296052c5fefe80a37236b7e3b1163a7a9b2a1
Andrej Streicher <andrejstreicher1@gmail.com>
2024-04-20 22:14:14 +0200

    Fit the base project into our outline

--- app/src/main/java/com/example/yourslovakiafrontend/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/yourslovakiafrontend/ui/search/SearchFragment.kt
@@ -1,4 +1,4 @@
-package com.example.yourslovakiafrontend.ui.dashboard
+package com.example.yourslovakiafrontend.ui.search
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.yourslovakiafrontend.databinding.FragmentDashboardBinding
+import com.example.yourslovakiafrontend.databinding.FragmentSearchBinding
 
-class DashboardFragment : Fragment() {
+class SearchFragment : Fragment() {
 
-private var _binding: FragmentDashboardBinding? = null
+private var _binding: FragmentSearchBinding? = null
   // This property is only valid between onCreateView and
   // onDestroyView.
   private val binding get() = _binding!!
@@ -21,14 +21,14 @@ private var _binding: FragmentDashboardBinding? = null
     container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View {
-    val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+    val searchViewModel =
+            ViewModelProvider(this).get(SearchViewModel::class.java)
 
-    _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+    _binding = FragmentSearchBinding.inflate(inflater, container, false)
     val root: View = binding.root
 
     val textView: TextView = binding.textDashboard
-    dashboardViewModel.text.observe(viewLifecycleOwner) {
+    searchViewModel.text.observe(viewLifecycleOwner) {
       textView.text = it
     }
     return root
