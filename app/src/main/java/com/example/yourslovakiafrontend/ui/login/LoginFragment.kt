655619604164542dc093665e0b50160b40391884
Tomas Kubrican <tomkubri@gmail.com>
2024-04-21 15:52:44 +0200

    Added confirm password field and logo

--- app/src/main/java/com/example/yourslovakiafrontend/ui/login/LoginFragment.kt
+++ app/src/main/java/com/example/yourslovakiafrontend/ui/login/LoginFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.yourslovakiafrontend.databinding.FragmentLoginBinding
 
@@ -22,13 +23,22 @@ class LoginFragment : Fragment() {
 
         val emailEditText = binding.editTextEmail
         val passwordEditText = binding.editTextPassword
+        val confirmPasswordEditText = binding.editTextConfirmPassword
         val loginButton = binding.buttonLogin
 
         loginButton.setOnClickListener {
             val email = emailEditText.text.toString()
             val password = passwordEditText.text.toString()
-            //add login verification
+            val confirmPassword = confirmPasswordEditText.text.toString()
+
+            if (password == confirmPassword) {
+                // Passwords match, proceed with login
+            } else {
+                // Passwords don't match, show an error message
+                Toast.makeText(requireContext(), "Passwords do not match", Toast.LENGTH_SHORT).show()
+            }
         }
+
 
         return root
     }
