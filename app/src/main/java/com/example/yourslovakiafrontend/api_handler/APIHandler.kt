3c3c13d6c40c781502e527a32152f9de225e6006
Andrej Streicher <andrejstreicher1@gmail.com>
2024-04-22 14:37:25 +0200

    Base register API call

--- app/src/main/java/com/example/yourslovakiafrontend/api_handler/APIHandler.kt
+++ app/src/main/java/com/example/yourslovakiafrontend/api_handler/APIHandler.kt
@@ -1,13 +1,25 @@
 package com.example.yourslovakiafrontend.api_handler
 
+import fiit.mtaa.yourslovakia.models.AuthenticationRequest
 import okhttp3.*
 import java.io.IOException
+import com.google.gson.Gson
+import fiit.mtaa.yourslovakia.models.AuthenticationResponse
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.RequestBody.Companion.toRequestBody
 
 val client = OkHttpClient()
+private val baseUrl = "yourslovakia.streicher.tech/"
+private var jwtToken = ""
+private var refreshToken = ""
+fun register(authenticationRequest: AuthenticationRequest) {
+    val jsonBody = Gson().toJson(authenticationRequest)
 
-fun run(url: String) {
+    val requestBody = jsonBody.toRequestBody("application/json".toMediaTypeOrNull())
+
     val request = Request.Builder()
-        .url(url)
+        .url(baseUrl + "create_user")
+        .post(requestBody)
         .build()
 
     client.newCall(request).enqueue(object : Callback {
@@ -16,7 +28,14 @@ fun run(url: String) {
         }
 
         override fun onResponse(call: Call, response: Response) {
-            println(response.body?.string())
+            if (response.isSuccessful) {
+                val responseData = response.body?.string()
+                if (responseData == "true") {
+                    println("Response is true")
+                }
+            } else {
+                println("Unsuccessful response")
+            }
         }
     })
 }
