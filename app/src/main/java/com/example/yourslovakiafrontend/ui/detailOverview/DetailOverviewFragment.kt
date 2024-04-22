828536d8141159e1c9259f3b8fd9c9a164cc6234
Tomas Kubrican <tomkubri@gmail.com>
2024-04-22 11:22:18 +0200

    Added xml files for favourites and home screens

---
+++ app/src/main/java/com/example/yourslovakiafrontend/ui/detailOverview/DetailOverviewFragment.kt
@@ -0,0 +1,10 @@
+package com.example.yourslovakiafrontend.ui.detailOverview
+
+import androidx.fragment.app.Fragment
+
+class DetailsFragment : Fragment() {
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+    }
+}
