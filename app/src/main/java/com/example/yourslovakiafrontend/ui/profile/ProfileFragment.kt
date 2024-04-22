096203e6c198e2db3b756a132eeef07496749113
Tomas Kubrican <tomkubri@gmail.com>
2024-04-22 21:08:47 +0200

    Added language changing

--- app/src/main/java/com/example/yourslovakiafrontend/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/yourslovakiafrontend/ui/profile/ProfileFragment.kt
@@ -1,37 +1,66 @@
 package com.example.yourslovakiafrontend.ui.profile
 
+import android.content.res.Configuration
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import com.example.yourslovakiafrontend.databinding.FragmentProfileBinding
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.PopupMenu
+import androidx.appcompat.app.AppCompatActivity
+import com.example.yourslovakiafrontend.R
+import java.util.Locale
 
-class ProfileFragment : Fragment() {
+class ProfileFragment : AppCompatActivity() {
 
-    private var _binding: FragmentProfileBinding? = null
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_profile)
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        val profileViewModel =
-            ViewModelProvider(this).get(ProfileViewModel::class.java)
-
-        _binding = FragmentProfileBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        return root
+        val changeLanguageButton = findViewById<Button>(R.id.button_change_language)
+        changeLanguageButton.setOnClickListener { showLanguagePopupMenu(changeLanguageButton) }
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    private fun showLanguagePopupMenu(anchorView: Button) {
+        val popupMenu = PopupMenu(this, anchorView)
+        popupMenu.menuInflater.inflate(R.menu.language_menu, popupMenu.menu)
+        popupMenu.setOnMenuItemClickListener { menuItem: MenuItem ->
+            when (menuItem.itemId) {
+                R.id.czech -> {
+                    setLocale("cs")
+                    true
+                }
+
+                R.id.german -> {
+                    setLocale("de")
+                    true
+                }
+
+                R.id.english -> {
+                    setLocale("en")
+                    true
+                }
+
+                R.id.slovak -> {
+                    setLocale("sk")
+                    true
+                }
+
+                else -> false
+            }
+        }
+
+        popupMenu.show()
+    }
+
+    private fun setLocale(languageCode: String) {
+        val locale = Locale(languageCode)
+        Locale.setDefault(locale)
+
+        val configuration = Configuration()
+        configuration.setLocale(locale)
+
+        resources.updateConfiguration(configuration, resources.displayMetrics)
+
+        // Restart the activity to apply the new locale
+        recreate()
     }
 }
