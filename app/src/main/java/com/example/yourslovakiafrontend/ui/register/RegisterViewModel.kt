82dd801cfcc7538ab0b87443f75328dd673e23f0
Andrej Streicher <andrejstreicher1@gmail.com>
2024-04-22 16:18:45 +0200

    Removed password length requirement

--- app/src/main/java/com/example/yourslovakiafrontend/ui/register/RegisterViewModel.kt
+++ app/src/main/java/com/example/yourslovakiafrontend/ui/register/RegisterViewModel.kt
@@ -15,9 +15,6 @@ class RegisterViewModel : ViewModel() {
         if (password != confirmPassword) {
             return false
         }
-        if (password.length < 8) {
-            return false
-        }
         return true
     }
 
