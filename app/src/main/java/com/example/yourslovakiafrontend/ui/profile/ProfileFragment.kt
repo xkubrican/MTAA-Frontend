ab92b7cb9c0df8b3de592b7ce75eff7fc7b8a929
Andrej Streicher <andrejstreicher1@gmail.com>
2024-04-22 22:07:47 +0200

    Working profile

--- app/src/main/java/com/example/yourslovakiafrontend/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/yourslovakiafrontend/ui/profile/ProfileFragment.kt
@@ -1,16 +1,7 @@
 package com.example.yourslovakiafrontend.ui.profile
 
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import com.example.yourslovakiafrontend.R
+import androidx.fragment.app.Fragment
 
-class ProfileFragment : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_profile)
-
-    }
-
+class ProfileFragment : Fragment() {
 
 }
